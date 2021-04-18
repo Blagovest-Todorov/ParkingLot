bf3938298cac3824cf5ebf66f11d7db7c4a9023a
Blagovest-Todorov <blagovest.stod@abv.bg>
2021-04-18 14:36:29 +0300

    add Program.cs

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ParkingLot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> carNumbers = new HashSet<string>();// create structure HashSet
+
+            while (true)
+            {
+                string line = Console.ReadLine();
+
+                if (line == "END")
+                {
+                    //ToDo
+                    if (carNumbers.Count!= 0)
+                    {
+                       PrintRemainingCars(carNumbers);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Parking Lot is Empty");
+                    }
+
+                    break;
+                }
+
+                string[] details = line.Split(", ");
+                string command = details[0];
+                string plateNumber = details[1];
+
+                if (command == "IN")
+                {
+                    carNumbers.Add(plateNumber);
+                }
+                else if (command == "OUT")
+                {
+                    carNumbers.Remove(plateNumber);
+                }
+            }
+        }
+
+        private static void PrintRemainingCars(HashSet<string> carNumbers)
+        {
+            foreach (var number in carNumbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
